0b39f624a100e0626c79c7db23502f0be79b7f59
Kodai-Java <kodai8250@gmail.com>
2025-03-20 23:26:46 +0900

    スケルトン作成

--- app/components/CompoundInterestCalculator/index.tsx
+++ app/components/CompoundInterestCalculator/index.tsx
@@ -1,7 +1,17 @@
 import { FC } from "react";
 
 export const CompoundInterestCalculator: FC = () => {
-  return <div>test</div>;
+  return (
+    <div>
+      <div>年率</div>
+      <div>積立年数</div>
+      <div>毎月積立額</div>
+      <div>積立前の元金</div>
+      <div>計算</div>
+      <div>クリア</div>
+      <div>結果</div>
+    </div>
+  );
 };
 
 export default CompoundInterestCalculator;
