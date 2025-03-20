88bec827fc90c34a3379769625d2dfeee3dda2a0
Kodai-Java <kodai8250@gmail.com>
2025-03-20 23:42:08 +0900

    flex当てた

--- app/components/CompoundInterestCalculator/index.tsx
+++ app/components/CompoundInterestCalculator/index.tsx
@@ -3,13 +3,17 @@ import { FC } from "react";
 export const CompoundInterestCalculator: FC = () => {
   return (
     <div>
-      <div>年率</div>
-      <div>積立年数</div>
-      <div>毎月積立額</div>
-      <div>積立前の元金</div>
-      <div>計算</div>
-      <div>クリア</div>
-      <div>結果</div>
+      <div className="flex flex-col">
+        <div>年率</div>
+        <div>積立年数</div>
+        <div>毎月積立額</div>
+        <div>積立前の元金</div>
+        <div className="flex flex-row">
+          <div>計算</div>
+          <div>クリア</div>
+        </div>
+        <div>結果</div>
+      </div>
     </div>
   );
 };
