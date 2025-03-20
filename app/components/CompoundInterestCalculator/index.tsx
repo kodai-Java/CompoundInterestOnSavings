188b2c0e1a8e71ab6d2773dffe0110bc6774f324
Kodai-Java <kodai8250@gmail.com>
2025-03-21 00:02:15 +0900

    テキストボックスとボタン配置

--- app/components/CompoundInterestCalculator/index.tsx
+++ app/components/CompoundInterestCalculator/index.tsx
@@ -1,16 +1,37 @@
+import { Button, TextField } from "@mui/material";
 import { FC } from "react";
 
 export const CompoundInterestCalculator: FC = () => {
   return (
     <div>
       <div className="flex flex-col">
-        <div>年率</div>
-        <div>積立年数</div>
-        <div>毎月積立額</div>
-        <div>積立前の元金</div>
+        <TextField
+          id="outlined-number"
+          label="年率"
+          type="number"
+          variant="standard"
+        />
+        <TextField
+          id="outlined-number"
+          label="積立年数"
+          type="number"
+          variant="standard"
+        />
+        <TextField
+          id="outlined-number"
+          label="毎月積立額"
+          type="number"
+          variant="standard"
+        />
+        <TextField
+          id="outlined-number"
+          label="積立前の元金"
+          type="number"
+          variant="standard"
+        />
         <div className="flex flex-row">
-          <div>計算</div>
-          <div>クリア</div>
+          <Button variant="contained">計算</Button>
+          <Button variant="outlined">クリア</Button>
         </div>
         <div>結果</div>
       </div>
