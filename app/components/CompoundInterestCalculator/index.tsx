b1fb75d7971b3d170c84d4759d06439fd4f6e0dd
Kodai-Java <kodai8250@gmail.com>
2025-03-21 00:35:46 +0900

    add calc function

--- app/components/CompoundInterestCalculator/index.tsx
+++ app/components/CompoundInterestCalculator/index.tsx
@@ -1,10 +1,18 @@
 import { Button, TextField } from "@mui/material";
-import { FC } from "react";
+import { FC, useState } from "react";
 
 export const CompoundInterestCalculator: FC = () => {
+  const [resultMoney, setResultMoney] = useState<number>();
+  /**
+   * 複利計算
+   */
+  const calculateCompoundInterest = () => {
+    // 年率、年数、積立前の元金を複利計算する
+    setResultMoney(0);
+  };
   return (
     <div>
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-2">
         <TextField
           id="outlined-number"
           label="年率"
@@ -19,21 +27,19 @@ export const CompoundInterestCalculator: FC = () => {
         />
         <TextField
           id="outlined-number"
-          label="毎月積立額"
-          type="number"
-          variant="standard"
-        />
-        <TextField
-          id="outlined-number"
           label="積立前の元金"
           type="number"
           variant="standard"
         />
         <div className="flex flex-row">
-          <Button variant="contained">計算</Button>
-          <Button variant="outlined">クリア</Button>
+          <Button variant="contained" onClick={calculateCompoundInterest}>
+            計算
+          </Button>
         </div>
-        <div>結果</div>
+        <div className="flex flex-row gap-1">
+          <div>結果</div>
+          <div>{resultMoney}円</div>
+        </div>
       </div>
     </div>
   );
