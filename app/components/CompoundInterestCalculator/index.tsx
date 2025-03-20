c7ad1f9bf667d690285ac4e2b89f6922e2fbc0ea
Kodai-Java <kodai8250@gmail.com>
2025-03-21 00:50:24 +0900

    fix css

--- app/components/CompoundInterestCalculator/index.tsx
+++ app/components/CompoundInterestCalculator/index.tsx
@@ -11,32 +11,30 @@ export const CompoundInterestCalculator: FC = () => {
     setResultMoney(0);
   };
   return (
-    <div>
-      <div className="flex flex-col gap-2">
+    <div className="flex justify-center">
+      <div className="flex flex-col gap-4 w-40 text-center">
         <TextField
           id="outlined-number"
-          label="年率"
+          label="年率(%)"
           type="number"
           variant="standard"
         />
         <TextField
           id="outlined-number"
-          label="積立年数"
+          label="積立年数(年)"
           type="number"
           variant="standard"
         />
         <TextField
           id="outlined-number"
-          label="積立前の元金"
+          label="積立前の元金(円)"
           type="number"
           variant="standard"
         />
-        <div className="flex flex-row">
-          <Button variant="contained" onClick={calculateCompoundInterest}>
-            計算
-          </Button>
-        </div>
-        <div className="flex flex-row gap-1">
+        <Button variant="contained" onClick={calculateCompoundInterest}>
+          計算
+        </Button>
+        <div className="flex flex-row gap-1 justify-center">
           <div>結果</div>
           <div>{resultMoney}円</div>
         </div>
